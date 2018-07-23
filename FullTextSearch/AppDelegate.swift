f550333bdf0f76cb7b9037f5b90b0368529ce685
Bas van der Hoek <bas@hoek.com>
2018-07-23 22:00:21 +0200

    Initial Commit

---
+++ FullTextSearch/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FullTextSearch
+//
+//  Created by Bas van der Hoek on 23/07/2018.
+//  Copyright © 2018 Bas van der Hoek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
